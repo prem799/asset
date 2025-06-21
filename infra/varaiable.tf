f2ea0f28e5721399be44b903c675f2da30819d39
prem799 <125148339+prem799@users.noreply.github.com>
2025-06-21 11:57:50 +0530

    Update varaiable.tf

--- infra/varaiable.tf
+++ infra/varaiable.tf
@@ -1,23 +1,5 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS region to deploy resources in"
   type        = string
   default     = "us-east-1"
-}
-
-variable "ami_id" {
-  description = "The AMI ID for EC2 instances"
-  type        = string
-  default     = "ami-0f88e80871fd81e91"
-}
-
-variable "instance_type" {
-  description = "The instance type for EC2 instances"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "Name tag for the first EC2 instance"
-  type        = string
-  default     = "chitti_robo"
 }
